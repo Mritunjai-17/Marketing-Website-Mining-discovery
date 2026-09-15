0f2661f461a874a6be8237ce7272e7ceae697a97
satinderkaurjnv-dot <satinderkaurjnv@gmail.com>
2026-09-15 11:10:27 +0530

    chore: update allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.4'],
+  allowedDevOrigins: ['192.168.1.4', '10.149.3.60'],
   devIndicators: false,
   images: {
     qualities: [75, 95],
